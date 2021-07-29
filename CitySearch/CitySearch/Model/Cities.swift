33758786ea297a9deb420265154732138409e802
DhananjayDubey <ddubey330@gmail.com>
2021-07-29 22:45:11 +0530

    Added cities.json file in app bundle Added model for cities Moved viewController in UI group

---
+++ CitySearch/CitySearch/Model/Cities.swift
@@ -0,0 +1,78 @@
+//
+//  Cities.swift
+//  CitySearch
+//
+//  Created by Dhananjay Dubey on 29/7/21.
+//
+
+import Foundation
+
+// MARK: - Cities
+
+/// Model to represent all the `cities`
+struct Cities: Codable {
+    let cities: [City]
+}
+
+// MARK: - City
+/**
+ Model to represent `City`
+ Marking all the properties nullable, because didn't wanted it to be tightly coupled. It might be parsing fail or crash, if any of the value comes as nil from backend. To avoid that, and to remove that dependency, thought to take everything as optional
+ */
+struct City: Codable {
+    let country: String?
+    let city: String?
+    let id: Int?
+    let coordinates: Coordinates?
+    
+    enum CodingKeys: String, CodingKey {
+        case country
+        case city = "name"
+        case id = "_id"
+        case coordinates = "coord"
+    }
+    
+    init(from decoder: Decoder) throws {
+        let values = try decoder.container(keyedBy: CodingKeys.self)
+        
+        self.country = try values.decodeIfPresent(String.self, forKey: .country)
+        self.city = try values.decodeIfPresent(String.self, forKey: .city)
+        self.id = try values.decodeIfPresent(Int.self, forKey: .id)
+        self.coordinates = try values.decodeIfPresent(Coordinates.self, forKey: .coordinates)
+    }
+}
+
+// MARK: - Coord
+struct Coordinates: Codable {
+    let lon, lat: Double
+}
+
+//typealias Cities = [City]
+
+extension City {
+    
+    /**
+     This will be combination of `City` and `Country` -> `City, Country`.
+     If `City` and `Country` is available, then it will return `City, Country`
+     for e.g
+     `City = Hurzuf` and `Country = UA`
+     then `fullAddress` will be `Hurzuf, UA`
+     
+     if `City = nil / ""` and `Country = UA` , then `fullAddress` will be ` UA`
+     if `City = Hurzuf` and `Country = nil / ""` , then `fullAddress` will be ` Hurzuf`
+     */
+    
+    var fullAddress: String {
+        var arr: [String] = []
+        
+        if let city = self.city,
+           !city.isEmpty {
+            arr.append(city)
+        }
+        if let country = self.country,
+           !country.isEmpty {
+            arr.append(country)
+        }
+        return arr.joined(separator: ", ")
+    }
+}
